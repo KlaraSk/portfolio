4b4aecc80db2393d57dee63da383a2b6a4d61d3f
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-08-17 21:39:36 +0200

    Variables and basic css setup fixed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div className="app">Hello portfolio</div>;
+  return (
+    <div className="app font-color-black">
+      <section className="page">
+        <h1 className="heading-1 ">Det här är en h1:a</h1>
+        <h2 className="heading-2">Det här är en h2:a</h2>
+        <h3 className="heading-3">Det här är en h3:a</h3>
+        <p className="body">Det här är en paragraph</p>
+      </section>
+    </div>
+  );
 }
 
 export default App;
